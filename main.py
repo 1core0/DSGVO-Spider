7b889792e47d751075763648ff48e261f0add31c
1core0 <akyuezah@gmail.com>
2023-04-01 01:15:20 +0200

    async request implemented for speed optimization

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import asyncio
-import aiohttp
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse, urljoin
 import argparse
-from tqdm import tqdm
+import asyncio
+from crawler import crawl
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--domain', required=True, help='Domain to crawl')
+parser.add_argument('--rate', type=int, default=20, help='Number of concurrent requests')
 args = parser.parse_args()
 
 # Set the domain to crawl
@@ -15,48 +13,7 @@ domain = args.domain
 
 # Set the initial URL to crawl
 url = 'https://' + domain
-visited_urls = set()
-
-# Create a queue to store sublinks
-sublinks_queue = [url]
-
-# Recursive function to crawl all links
-async def crawl(url):
-    visited_urls.add(url)
-    print("Crawling: ", url)
-
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(url, timeout=10) as response:
-                content = await response.read()
-        except asyncio.TimeoutError:
-            print(f"Timed out while requesting: {url}")
-            return
-
-    soup = BeautifulSoup(content, 'html.parser')
-    links = soup.find_all('a')
-    for link in links:
-        href = link.get('href')
-        if href is None:
-            continue
-
-        # Construct the absolute URL of the link
-        abs_url = urljoin(url, href)
-        parsed_url = urlparse(abs_url)
-
-        # Follow links to subdomains and other domains
-        if parsed_url.netloc.endswith(domain) and abs_url not in visited_urls and abs_url not in sublinks_queue:
-            sublinks_queue.append(abs_url)
-            print("Found sublink: ", abs_url)
-
-    # Recursively crawl sublinks
-    tasks = []
-    for sublink in sublinks_queue:
-        if sublink not in visited_urls:
-            task = asyncio.create_task(crawl(sublink))
-            tasks.append(task)
-    await asyncio.gather(*tasks)
 
 # Start crawling all sublinks in the queue
-asyncio.run(crawl(url))
+asyncio.run(crawl(url, domain, args.rate))
 
