bb93c35e7bec82f78a0c66681ea63dba84dedca4
1core0 <akyuezah@gmail.com>
2023-03-31 19:07:32 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,92 +1,51 @@
 import requests
-import sqlite3
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+import argparse
+from tqdm import tqdm
 
-# Connect to SQLite database
-conn = sqlite3.connect('sqllite.db')
-c = conn.cursor()
-
-# Create cookies table if it doesn't exist
-c.execute('''CREATE TABLE IF NOT EXISTS cookies
-             (url TEXT, name TEXT, value TEXT)''')
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--domain', required=True, help='Domain to crawl')
+args = parser.parse_args()
 
 # Set the domain to crawl
-domain = 'example.com'
-
-# Set the cookie policy
-cookie_policy = 'selective'  # Options: 'accept', 'decline', 'selective'
+domain = args.domain
 
 # Set the initial URL to crawl
 url = 'https://' + domain
 visited_urls = set()
 
+# Create a queue to store sublinks
+sublinks_queue = [url]
+
 # Recursive function to crawl all links
 def crawl(url):
-    if url in visited_urls:
-        return
     visited_urls.add(url)
-    
-    # Send a GET request to the URL and get the HTML content
-    try:
-        response = requests.get(url, timeout=5)
-    except requests.exceptions.Timeout:
-        print("Request timed out for URL:", url)
-        return
-    except requests.exceptions.ConnectionError:
-        print("Failed to connect to URL:", url)
-        return
-
+    print("Crawling: ", url)
+    response = requests.get(url, timeout=5)
     soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Extract all links from the HTML content
     links = soup.find_all('a')
     for link in links:
         href = link.get('href')
         if href is None:
             continue
-        
+
         # Construct the absolute URL of the link
         abs_url = urljoin(url, href)
         parsed_url = urlparse(abs_url)
 
         # Follow links to subdomains and other domains
-        if parsed_url.netloc.endswith(domain):
-            crawl(abs_url)
-        else:
-            print("Following link to external domain:", abs_url)
-            try:
-                response = requests.get(abs_url, timeout=5)
-                print("Visited external URL:", abs_url)
-            except requests.exceptions.Timeout:
-                print("Request timed out for URL:", abs_url)
-            except requests.exceptions.ConnectionError:
-                print("Failed to connect to URL:", abs_url)
-            continue
-        
-        # Store cookies in the database
-        cookies = response.cookies
-        for cookie in cookies:
-            cookie_name = cookie.name
-            cookie_value = cookie.value
-            
-            if cookie_policy == 'accept':
-                c.execute('INSERT INTO cookies (url, name, value) VALUES (?, ?, ?)', (abs_url, cookie_name, cookie_value))
-            elif cookie_policy == 'decline':
-                continue
-            elif cookie_policy == 'selective':
-                print('Do you want to accept the following cookie?')
-                print('URL:', abs_url)
-                print('Name:', cookie_name)
-                print('Value:', cookie_value)
-                choice = input('Enter "y" to accept or "n" to decline: ')
-                if choice == 'y':
-                    c.execute('INSERT INTO cookies (url, name, value) VALUES (?, ?, ?)', (abs_url, cookie_name, cookie_value))
-    
-    conn.commit()
+        if parsed_url.netloc.endswith(domain) and abs_url not in visited_urls and abs_url not in sublinks_queue:
+            sublinks_queue.append(abs_url)
+            print("Found sublink: ", abs_url)
 
-# Start crawling the initial URL
+    # Recursively crawl sublinks
+    for sublink in sublinks_queue:
+        if sublink not in visited_urls:
+            crawl(sublink)
+
+# Start crawling all sublinks in the queue
 crawl(url)
 
-# Close the database connection
-conn.close()
+
